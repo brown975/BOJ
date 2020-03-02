a32f2e7e970dd2523658423567f56dae2013cb25
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-02 17:13:36 +0900

    Create 2441.cpp

---
+++ 2441.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int a, s, d;
+int main(void) {
+	scanf("%d", &a);
+	for (int g = 0, h; g < a; g++) {
+		h = 0;
+		for (; h < g; h++)
+			printf(" ");
+		h = g;
+		for (; h < a; h++)
+			printf("*");
+		printf("\n");
+	}
+}
