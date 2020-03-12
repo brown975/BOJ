8374b65981d7df545a6f6023951d29bc90c97944
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-12 22:50:02 +0900

    Create 3009.cpp

---
+++ 3009.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int a, s, d, f, g, h;
+int main() {
+    scanf("%d%d%d%d%d%d", &a, &s, &d, &f, &g, &h);
+    if (a - d) {
+        if (g - d) {
+            if (s - f)
+                printf("%d %d", d, h);
+            else
+                printf("%d %d", d, h);
+        }
+        else {
+            if (s - f)
+                printf("%d %d", a, f);
+            else
+                printf("%d %d", a, h);
+        }
+    }
+    else {
+        if (s - h)
+            printf("%d %d", g, s);
+        else
+            printf("%d %d", g, f);
+    }
+}
