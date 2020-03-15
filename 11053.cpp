94a49f6e31021ec1ccc1cea876cea5ba5a2eb901
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-15 14:39:55 +0900

    Create 11053.cpp

---
+++ 11053.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int a[1005] = { 0 }, s[1005] = { 0 }, d, f, k = 0;
+int main(void) {
+	cin >> d;
+	for (int g = 1; g <= d; g++) {
+		cin >> a[g];
+	}
+	for (int g = 1; g <= d; g++) {
+		f = 0;
+		for (int h = 1; h < g; h++) {
+			if ((a[h] < a[g]) && (f < s[h]))
+				f = s[h];
+		}
+		s[g] = f + 1;
+		if (k < s[g]) {
+			k = s[g];
+		}
+	}
+	cout << k;
+}
