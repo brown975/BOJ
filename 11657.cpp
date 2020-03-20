faebb38787e9540e36fcc511732501292fba7d4c
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-20 19:51:31 +0900

    Update 11657.cpp

--- 11657.cpp
+++ 11657.cpp
@@ -1,39 +1,28 @@
 #include <stdio.h>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int a, s, d[501] = { 2000000000 }, t = 0;
-vector<pair<int, int>> f[501];
+int a, s, d[501], f[6001][3];
 int main() {
     scanf("%d%d", &a, &s);
-    for (int g = 0, w, e, r; g < s; g++) {
+    for (int g = 1, w, e, r; g <= s; g++) {
         scanf("%d%d%d", &w, &e, &r);
-        f[w].push_back({ e, r });
+        f[g][0] = w;
+        f[g][1] = e;
+        f[g][2] = r;
     }
-    fill(d, d + 501, 2000000000);
     d[1] = 0;
-    for (int g = 1; g <= a; g++) {
-        for (int h = 1; h <= a; h++) {
-            for (int j = 0; j < f[h].size(); j++) {
-                if (d[f[h][j].first] > d[h] + f[h][j].second) {
-                    d[f[h][j].first] = d[h] + f[h][j].second;
-                    if (g == a)
-                        t = 1;
-                }
+    for (int g = 2; g <= a; g++)
+        d[g] = 2000000000;
+    for (int g = 1; g <= a - 1; g++)
+        for (int h = 1; h <= s; h++)
+            if (d[f[h][0]] - 2000000000) {
+                int t = d[f[h][0]] + f[h][2];
+                if (d[f[h][1]] > t)
+                    d[f[h][1]] = t;
             }
+    for (int g = 1; g <= s; g++)
+        if (d[f[g][0]] - 2000000000 && d[f[g][1]] > d[f[g][0]] + f[g][2]) {
+            printf("-1");
+            return 0;
         }
-    }
-    if (t) {
-        printf("-1");
-    }
-    else {
-        for (int g = 2; g <= a; g++) {
-            if (d[g] == 2000000000) {
-                printf("-1\n");
-            }
-            else {
-                printf("%d\n", d[g]);
-            }
-        }
-    }
+    for (int g = 2; g <= a; g++)
+        printf("%d\n", (d[g] - 2000000000) ? d[g] : -1);
 }
