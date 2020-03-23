69e2e1b2606d481197bd230571525b1b46785eb6
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-23 19:16:27 +0900

    Create 5565.cpp

---
+++ 5565.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int a, s, d, f, g, h, j, k, l, q;
+int main() {
+	scanf("%d%d%d%d%d%d%d%d%d%d", &a, &s, &d, &f, &g, &h, &j, &k, &l, &q);
+	printf("%d", a - s - d - f - g - h - j - k - l - q);
+}
