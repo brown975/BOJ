7fde29bf9e93db2d9995372fcfe2a81677abb66e
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-24 13:21:03 +0900

    Create 15663.cpp

---
+++ 15663.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+int a, s, d[9], w[9];
+set<vector<int>> f;
+vector<int> t(9);
+void asdf(int r) {
+	if (r == s) {
+		f.insert(t);
+		return;
+	}
+	for (int g = 0; g < a; g++) {
+		if (!w[g]) {
+			t[r] = d[g];
+			w[g] = 1;
+			asdf(r + 1);
+			w[g] = 0;
+		}
+	}
+}
+int main() {
+	scanf("%d%d", &a, &s);
+	for (int g = 0; g < a; g++)
+		scanf("%d", &d[g]);
+	sort(d, d + a);
+	asdf(0);
+	for (auto g : f) {
+		for (int h = 0; h < s; h++)
+			printf("%d ", g[h]);
+		puts("");
+	}
+}
