8866e32c0063d83f85f1875753edb346c69966e4
brown975 <61533223+brown975@users.noreply.github.com>
2020-03-24 13:05:37 +0900

    Create 15655.cpp

---
+++ 15655.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int a, s, d[9], t[9];
+void asdf(int e, int r) {
+	if (r == s) {
+		for (int g = 0; g < s; g++)
+			printf("%d ", t[g]);
+		puts("");
+		return;
+	}
+	for (int g = e + 1; g < a; g++) {
+		t[r] = d[g];
+		asdf(g, r + 1);
+	}
+}
+int main() {
+	scanf("%d%d", &a, &s);
+	for (int g = 0; g < a; g++)
+		scanf("%d", &d[g]);
+	sort(d, d + a);
+	asdf(-1, 0);
+}
