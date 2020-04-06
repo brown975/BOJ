e1ed05c94cb2f82e6a81cf19248e7e899255717f
brown975 <61533223+brown975@users.noreply.github.com>
2020-04-06 19:14:30 +0900

    Update 1504.cpp

--- 1504.cpp
+++ 1504.cpp
@@ -1,33 +1,22 @@
 #include <stdio.h>
-#include <vector>
 #include <algorithm>
 using namespace std;
-int a, s;
+long long a, s, d[801][801], f, q, w;
 int main() {
-	scanf("%d%d", &a, &s);
-	vector<vector<int>> q(a, vector<int>(a, 10000000));
-	int w, e, r;
-	for (int g = 0; g < s; g++) {
-		scanf("%d%d%d", &w, &e, &r);
-		w--;
-		e--;
-		q[w][e] = r;
-		q[e][w] = r;
-	}
-	for (int g = 0; g < a; g++)
-		q[g][g] = 0;
-	for (int j = 0; j < a; j++) {
-		for (int g = 0; g < a; g++) {
-			for (int h = 0; h < a; h++) {
-				q[g][h] = min(q[g][h], q[g][j] + q[j][h]);
-			}
-		}
-	}
-	scanf("%d%d", &w, &e);
-	w--;
-	e--;
-	int t = min(q[0][w] + q[w][e] + q[e][a - 1], q[0][e] + q[e][w] + q[w][a - 1]);
-	if (t > 10000000)
-		t = -1;
-	printf("%d", t);
+    scanf("%lld%lld", &a, &s);
+    for (long long g = 1; g <= a; d[g][g] = 0, g++)
+        for (long long h = 1; h <= a; h++)
+            d[g][h] = 2000000000;
+    for (long long g = 0, w, e, r; g < s; d[w][e] = min(d[w][e], r), d[e][w] = min(d[e][w], r), g++)
+        scanf("%lld%lld%lld", &w, &e, &r);
+    scanf("%lld%lld", &q, &w);
+    for (long long g = 1; g <= a; g++)
+        for (long long h = 1; h <= a; h++)
+            for (long long j = 1; j <= a; j++)
+                d[h][j] = min(d[h][j], d[h][g] + d[g][j]);
+    f = min(d[1][q] + d[q][w] + d[w][a], d[1][w] + d[q][w] + d[q][a]);
+    if (f >= 2000000000 || !s)
+        printf("-1");
+    else
+        printf("%lld", f);
 }
