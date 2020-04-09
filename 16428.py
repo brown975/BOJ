11a2b89c95515e96c1a42eae38b83248e0818b9b
brown975 <61533223+brown975@users.noreply.github.com>
2020-04-09 21:53:42 +0900

    Update 16428.py

--- 16428.py
+++ 16428.py
@@ -1,3 +1,7 @@
 a=list(map(int,input().split()))
-print(a[0]//a[1])
-print(a[0]%a[1])
+if a[1]<0:
+    print(a[0]//a[1]+1)
+    print(a[0]%a[1]-a[1])
+else:
+    print(a[0]//a[1])
+    print(a[0]%a[1])
