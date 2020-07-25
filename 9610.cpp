9fc56746fc4bb996bda2f1b046f2d4ee0bf5f6f2
brown975 <61533223+brown975@users.noreply.github.com>
2020-07-25 22:15:52 +0900

    Create 9610.cpp

---
+++ 9610.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int a, s, d, f, q, w, e, r;
+int main() {
+	scanf("%d", &a);
+	for (int g = 1; g <= a; g++) {
+		scanf("%d%d", &s, &d);
+		(!s || !d ? f : s < 0 ? d < 0 ? e : w : d < 0 ? r : q)++;
+	}
+	printf("Q1: %d\nQ2: %d\nQ3: %d\nQ4: %d\nAXIS: %d", q, w, e, r, f);
+}
